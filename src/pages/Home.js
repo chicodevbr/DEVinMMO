5575cdfbc1a6c6e236c7dec1efc8ef368730561c
chicodevbr <chicodevbr@gmail.com>
2021-11-10 22:25:18 -0300

    create finder/search with useState

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,10 @@
 import React, { useEffect, useState } from 'react';
 import api from '../api/service';
 import GamesList from '../components/Games/GamesList';
-import SearchBar from '../components/Search/SearchBar';
-import { SearchBarContainer } from '../components/Search/SearchBarStyled';
+import GamesFinder from '../components/Search/GamesFinder';
 
 const Home = () => {
   const [games, setGames] = useState([]);
-  const [inputValue, setInputValue] = useState(null);
 
   useEffect(() => {
     api.get('games').then((response) => {
@@ -14,32 +12,10 @@ const Home = () => {
     });
   }, []);
 
-  const changeInputHandler = (event) => {
-    event.preventDefault();
-
-    setInputValue(event.target.value);
-  };
-
-  console.log(inputValue);
-  const loadedGames = games.filter((game) => game.title === inputValue);
-
-  if (inputValue === null || '' || undefined) {
-    return (
-      <main>
-        <SearchBarContainer>
-          <SearchBar onChange={changeInputHandler} />
-        </SearchBarContainer>
-        <GamesList items={games} />
-      </main>
-    );
-  }
-
   return (
     <main>
-      <SearchBarContainer>
-        <SearchBar onChange={changeInputHandler} />
-      </SearchBarContainer>
-      <GamesList items={loadedGames} />
+      <GamesFinder items={games} />
+      <GamesList items={games} />
     </main>
   );
 };
