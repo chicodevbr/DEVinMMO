9ee8315a1607bd7a88b2bef0cc77d06e9d12fb53
chicodevbr <chicodevbr@gmail.com>
2021-11-15 18:38:58 -0300

    fixed size

--- src/components/Navigation/Title.js
+++ src/components/Navigation/Title.js
@@ -1,15 +1,11 @@
 import styled from 'styled-components';
 
 const TitleStyled = styled.h1`
-  display: none;
-
   @media (min-width: 416px) {
-    display: block;
     margin: 2rem;
   }
 
   @media (min-width: 1145px) {
-    display: block;
     margin-right: 5rem;
   }
 `;
