096a81e4e836433383359dea669b476d4e76497a
chicodevbr <chicodevbr@gmail.com>
2021-11-17 17:45:26 -0300

    clear tooglebutton

--- src/components/Theme/ThemeButtonStyled.js
+++ src/components/Theme/ThemeButtonStyled.js
@@ -1,9 +1,6 @@
 import styled from 'styled-components';
 
 export const ThemeButton = styled.div`
-  width: 2rem;
-  height: 2rem;
-
   transition: all 200ms ease-in-out;
   cursor: pointer;
 `;
--- src/components/Theme/ThemeToggle.js
+++ src/components/Theme/ThemeToggle.js
@@ -1,25 +1,21 @@
 import React from 'react';
 import { useThemeToggle } from '../../context/AppThemeProvider';
-import { Flex, View, ToggleButton } from '@adobe/react-spectrum';
+import { ToggleButton } from '@adobe/react-spectrum';
 
 import Moon from '@spectrum-icons/workflow/Moon';
 
 const ThemeToggle = (props) => {
   const { toggleTheme } = useThemeToggle();
   return (
-    <Flex wrap grid="size-10">
-      <View>
-        <ToggleButton
-          staticColor={'white'}
-          width={{ base: '40px' }}
-          onPress={toggleTheme}
-          onClick={props.onClick}
-          isQuiet
-        >
-          <Moon aria-label="Switch to Dark Mode" />
-        </ToggleButton>
-      </View>
-    </Flex>
+    <ToggleButton
+      staticColor={'white'}
+      width={{ base: '40px' }}
+      onPress={toggleTheme}
+      onClick={props.onClick}
+      isQuiet
+    >
+      <Moon aria-label="Switch to Dark Mode" />
+    </ToggleButton>
   );
 };
 
